8cd67824aeb0ab59f854a27a9893693ec2458557
FadjarMaulana <fadjar.maulana15@gmail.com>
2020-03-05 14:36:13 +0700

    navbar

---
+++ js/htmljavascript.js
@@ -0,0 +1,56 @@
+var fluid1 = document.querySelectorAll('div')[0]
+fluid1.className = "container-fluid"
+
+var divrow1 = document.querySelectorAll('div')[1]
+divrow1.className = "row"
+
+var divcol1 = document.querySelectorAll('div')[2]
+divcol1.className = "col-sm-12"
+
+var nav1 = document.querySelectorAll('nav')[0]
+nav1.className = "navbar navbar-expand-lg navbar-light bg-primary fixed-top"
+
+var a1 = document.querySelectorAll('a')[0]
+a1.className = "navbar-brand"
+
+var button = document.querySelectorAll('button')[0]
+button.className = "navbar-toggler"
+
+var span = document.querySelectorAll('span')[0]
+span.className = "navbar-toggler-icon"
+
+var divcoll = document.querySelectorAll('div')[3]
+divcoll.className = "collapse navbar-collapse"
+
+var ul1 = document.querySelectorAll('ul')[0]
+ul1.className = "navbar-nav ml-auto"
+
+var li1 = document.querySelectorAll('li')[0]
+li1.className = "nav-item"
+
+var li2 = document.querySelectorAll('li')[1]
+li2.className = "nav-item"
+
+var li3 = document.querySelectorAll('li')[2]
+li3.className = "nav-item"
+
+var li4 = document.querySelectorAll('li')[3]
+li4.className = "nav-item"
+
+var li5 = document.querySelectorAll('li')[4]
+li5.className = "nav-item"
+
+var a2 = document.querySelectorAll('a')[1]
+a2.className = "nav-link mr-4"
+
+var a3 = document.querySelectorAll('a')[2]
+a3.className = "nav-link mr-4"
+
+var a4 = document.querySelectorAll('a')[3]
+a4.className = "nav-link mr-4"
+
+var a5 = document.querySelectorAll('a')[4]
+a5.className = "nav-link mr-4"
+
+var a6 = document.querySelectorAll('a')[5]
+a6.className = "nav-link mr-4"
