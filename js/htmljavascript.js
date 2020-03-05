dcec6aada675a3631cd82a1530d8227cc5a3e0f5
FadjarMaulana <fadjar.maulana15@gmail.com>
2020-03-05 17:04:50 +0700

    done

--- js/htmljavascript.js
+++ js/htmljavascript.js
@@ -54,3 +54,90 @@ a5.className = "nav-link mr-4"
 
 var a6 = document.querySelectorAll('a')[5]
 a6.className = "nav-link mr-4"
+
+var divcarousel1 = document.querySelectorAll('div')[4]
+divcarousel1.className = "carousel slide"
+
+var divcarousel2 = document.querySelectorAll('div')[5]
+divcarousel2.className = "carousel-inner"
+
+var divcarousel3 = document.querySelectorAll('div')[6]
+divcarousel3.className = "carousel-item active"
+
+var img1 = document.querySelectorAll('img')[0]
+img1.className = "d-block w-100"
+
+var divcarousel4 = document.querySelectorAll('div')[7]
+divcarousel4.className = "carousel-caption"
+
+var divcarousel5 = document.querySelectorAll('div')[8]
+divcarousel5.className = "carousel-item"
+
+var img2 = document.querySelectorAll('img')[1]
+img2.className = "d-block w-100"
+
+var divcarousel6 = document.querySelectorAll('div')[9]
+divcarousel6.className = "carousel-caption"
+
+var divcarousel7 = document.querySelectorAll('div')[10]
+divcarousel7.className = "carousel-item"
+
+var img3 = document.querySelectorAll('img')[2]
+img3.className = "d-block w-100"
+
+var divcarousel8 = document.querySelectorAll('div')[11]
+divcarousel8.className = "carousel-caption"
+
+var section1 = document.querySelectorAll('section')[0]
+section1.className = "about"
+
+var container1 = document.querySelectorAll('div')[12]
+container1.className = "container"
+
+var divrow2 = document.querySelectorAll('div')[13]
+divrow2.className = "row"
+
+var divcolm61 = document.querySelectorAll('div')[14]
+divcolm61.className = "col m6 light"
+
+var divcolm62 = document.querySelectorAll('div')[15]
+divcolm62.className = "col m6 light"
+
+var progres1 = document.querySelectorAll('div')[16]
+progres1.className = "progress"
+
+var progresbar1 = document.querySelectorAll('div')[17]
+progresbar1.className = "progress-bar"
+
+var progres2 = document.querySelectorAll('div')[18]
+progres2.className = "progress"
+
+var progresbar2 = document.querySelectorAll('div')[19]
+progresbar2.className = "progress-bar"
+
+var progres3 = document.querySelectorAll('div')[20]
+progres3.className = "progress"
+
+var progresbar3 = document.querySelectorAll('div')[21]
+progresbar3.className = "progress-bar"
+
+var fluid2 = document.querySelectorAll('div')[22]
+fluid2.className = "jumbotron jumbotron-fluid"
+
+var container2 = document.querySelectorAll('div')[23]
+container2.className = "container"
+
+var h3 = document.querySelectorAll('h3')[1]
+h3.className = "center light white-text"
+
+var divrow3 = document.querySelectorAll('div')[24]
+divrow3.className = "row"
+
+var divcolm41 = document.querySelectorAll('div')[25]
+divcolm41.className = "col m4 s12 center"
+
+var divcolm42 = document.querySelectorAll('div')[26]
+divcolm42.className = "col m4 s12 center"
+
+var divcolm43 = document.querySelectorAll('div')[27]
+divcolm43.className = "col m4 s12 center"
